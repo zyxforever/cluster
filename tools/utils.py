12fa10622327feada1e861de37950f106c61fb14
zyxforever <384498377@163.com>
2021-05-14 10:33:51 +0800

    dac

---
+++ tools/utils.py
@@ -0,0 +1,13 @@
+import numpy as np 
+from sklearn.utils.linear_assignment_ import linear_assignment
+
+def ACC(y_true, y_pred):
+    y_true = y_true.astype(np.int64)
+    assert y_pred.size == y_true.size
+    D = max(y_pred.max(), y_true.max()) + 1
+    w = np.zeros((D, D), dtype=np.int64)
+    for i in range(y_pred.size):
+        w[y_pred[i], y_true[i]] += 1
+    ind = linear_assignment(w.max() - w)
+
+    return sum([w[i, j] for i, j in ind]) * 1.0 / y_pred.size
